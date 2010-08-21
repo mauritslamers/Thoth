8329bd93b4a2cb9157346efcf54e381382fc2dbd
Maurits Lamers <maurits@weidestraat.nl>
2010-08-21 22:05:09 +0200

    Removing model sample as it deprecated

--- myModel.sample.js
+++
@@ -1,8 +0,0 @@
-var OrionModel = require('./OrionModel').Model;
-
-exports.myModel = OrionModel.extend({
-   bucket: 'model',
-   firstName: OrionModel.attr(String),
-   inbetween: OrionModel.attr(String),
-   lastname: OrionModel.attr(String)   
-});
