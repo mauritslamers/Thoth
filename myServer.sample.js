8eebbf42ee3932ea70ea99cc7cbe9569c3cef00f
Maurits Lamers <maurits@weidestraat.nl>
2011-02-20 15:00:27 +0100

    fixing a typo (thanks to ralfschimmel). Solves issue #1

--- myServer.sample.js
+++ myServer.sample.js
@@ -23,7 +23,7 @@ var myServer = Thoth.Server.create({
    port: 8080,
    //store: Thoth.Store.create(), // don't use directly!
    //store: RiakStore.create(),
-   //store: MySQLStoreMySQLClient.create({host:'', username:'', passwd: '', database: ''}),
+   //store: MySQLStoreMySQLClient.create({hostname:'', username:'', passwd: '', database: ''}),
    authModule: Thoth.FileAuth.create({ fileName: './myUsers'}),
    sessionModule: Thoth.Session.create({ sessionName: 'ThothServer' }),
    policyModule: Thoth.Policies.create({ policyFile: './myPolicies'})
