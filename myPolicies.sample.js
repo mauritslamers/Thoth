4f8ccf1766567d5d19f6151e6ed1ee7705f391b6
Maurits Lamers <maurits@weidestraat.nl>
2011-02-28 16:09:40 +0100

    Adding policy reloading, fixing policy bugs, creating store request creators as mixin

--- myPolicies.sample.js
+++ myPolicies.sample.js
@@ -5,13 +5,13 @@ if(!global.SC) require('./sc/runtime/core');
 // as it will ensure that the models are defined when the policies are loaded.
 // Make sure though your extensions are defined in the global name space!
 // If you rather choose not to define your extensions global, you have to require your extensions inside the policy files
-require('./PolicyModel');
+require('./lib/core/PolicyModel');
 
 // define the path where to look for policies:
-exports.policyPath = './policies';
+exports.policyPath = 'policies';
 
 // define which roles do not require policy checking: "root admin".w()
-// if you leave this emply every role will be checked
+// if you leave this empty every role will be checked
 exports.noPolicyCheckForRoles = "root admin".w();
 
 // define the resources you want to be able to load. Just define the resources.
