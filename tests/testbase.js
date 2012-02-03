2531c1978cab3bb87c56683156d5f925b0681837
Maurits Lamers <maurits@weidestraat.nl>
2012-02-03 20:55:43 +0100

    adding computed property check automation

--- tests/testbase.js
+++ tests/testbase.js
@@ -11,3 +11,20 @@ exports.inspect = sys.inspect;
 
 
 // test requests
+exports.objectHasComputedProperty = function(prop,cacheable){
+  return function(t){
+    assert.isFunction(t[prop]);
+    assert.isTrue(t[prop].isProperty);
+    if(cacheable) assert.isTrue(t[prop].isCacheable);
+    else assert.isUndefined(t[prop].isCacheable);
+  };
+};
+
+exports.classHassComputedProperty = function(prop,cacheable){
+  return function(t){
+    assert.isFunction(t.prototype[prop]);
+    assert.isTrue(t.prototype[prop].isProperty);
+    if(cacheable) assert.isTrue(t.prototype[prop].isCacheable);
+    else assert.isUndefined(t.prototype[prop].isCacheable);
+  };
+};
