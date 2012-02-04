b11c886386760a8e7ea29f17e48fce01d0f0bed3
Maurits Lamers <maurits@weidestraat.nl>
2012-02-04 21:18:10 +0100

    adding new tests for fetchRelation

--- tests/unittests/junctionrelations/relation_responses.js
+++ tests/unittests/junctionrelations/relation_responses.js
@@ -10,6 +10,7 @@ var stores = [];
 
 var mainBucket = 'student';
 var relationBucket = 'exam';
+var junctionBucket = 'exam_student';
 
 var makeStoreReq = function(data,method){
   var APIReq = Thoth.API.APIRequest.from(data,C.SOURCE_THOTH,method);
@@ -19,7 +20,7 @@ var makeStoreReq = function(data,method){
 var getStoreWith = function(cb){
   var store = FakeStore.create({
     cb: function(sr,client,callback){
-      cb(null,sr);
+      cb(null,sr,callback);
     }
   });
   stores.push(store);
@@ -36,8 +37,7 @@ junctionrelationstest.addBatch({
     'isMaster and isDirectRelation set to true': {
       topic: function(){
         var store = getStoreWith(this.callback);
-        var req = createRequest();
-        var sr = makeStoreReq(req,C.ACTION_FETCH);
+        var sr = makeStoreReq(createRequest(),C.ACTION_FETCH);
         sr.relations[0].type = 'toMany';
         sr.relations[0].isMaster = true;
         sr.relations[0].isDirectRelation = true;
@@ -58,11 +58,10 @@ junctionrelationstest.addBatch({
 }).addBatch({
 
   'fetchRelation given an relation with' : {
-    'isChildRecord set to true and type toOne should retrieve a single record': {
+    'isChildRecord and isMaster set to true and type toOne should retrieve a single record': {
       topic: function(){
         var store = getStoreWith(this.callback);
-        var req = createRequest();
-        var sr = makeStoreReq(req,C.ACTION_FETCH);
+        var sr = makeStoreReq(createRequest(),C.ACTION_FETCH);
         sr.relations[0].type = 'toOne';
         sr.relations[0].keys = 1;
         sr.relations[0].isMaster = true;
@@ -74,6 +73,45 @@ junctionrelationstest.addBatch({
         assert.strictEqual(t.get('requestType'), C.ACTION_REFRESH);
         assert.equal(t.key, 1);
         assert.strictEqual(t.bucket,relationBucket);
+      }
+    },
+    
+    // this should first fetch the relation record from the junction table, then actually retrieve the record if found
+    'isChildRecord and isMaster set to false and type toOne should first fetch the junction table, then retrieve the record': {
+      topic: function(){
+        var store = getStoreWith(this.callback);
+        var sr = makeStoreReq(createRequest(),C.ACTION_FETCH);
+        sr.relations[0].type = 'toOne';
+        sr.relations[0].keys = 1;
+        sr.relations[0].isMaster = false;
+        sr.relations[0].isDirectRelation = true;
+        store.fetchRelation(sr,{id: 1}, sr.relations[0],emptyFunc);
+      },
+      
+      'should call retrieveDBRecord with the relation request': function(t,cb){
+        var reqt = t.get('requestType');
+        assert.isTrue( (reqt === C.ACTION_FETCH) || (reqt === C.ACTION_REFRESH) );
+        if(t.get('requestType') === C.ACTION_FETCH){ // junction stuff
+          assert.strictEqual(t.bucket,junctionBucket);
+          assert.isTrue(t.conditions.search('student_id') !== -1);
+          assert.deepEqual(t.parameters.keys, [1]);
+          cb([{'student_id': 1, 'exam_id': 1}]);
+        }
+        else {
+          assert.strictEqual(t.bucket,relationBucket);
+          assert.strictEqual(t.key,1);
+        }
+      }
+    },    
+    
+    'isMaster set to true and type toOne should fetch a junction table record': {
+      topic: function(){
+        var store = getStoreWith(this.callback);
+        var sr = makeStoreReq(createRequest(),C.ACTION_FETCH);
+        sr.relations[0].type = 'toOne';
+        sr.relations[0].keys = 1;
+        sr.relations[0].isMaster = true;
+        store.fetchRelation(sr,{id: 1}, sr.relations[0],emptyFunc);         
       }
     }
   }
