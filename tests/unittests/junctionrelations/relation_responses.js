0486da76f5c0a2ce149848bd269f352799df4536
Maurits Lamers <maurits@weidestraat.nl>
2012-02-04 12:26:24 +0100

    fixing merge conflict

--- tests/unittests/junctionrelations/relation_responses.js
+++ tests/unittests/junctionrelations/relation_responses.js
@@ -8,9 +8,12 @@ var FakeStore = require('./fake_store').FakeStore;
 
 var stores = [];
 
-var makeStoreReq = function(data){
-  var APIReq = Thoth.APIRequest.from(data);
-  return Thoth.StoreRequest.from(APIReq);
+var mainBucket = 'student';
+var relationBucket = 'exam';
+
+var makeStoreReq = function(data,method){
+  var APIReq = Thoth.API.APIRequest.from(data,C.SOURCE_THOTH,method);
+  return Thoth.API.StoreRequest.from(APIReq);
 };
 
 var getStoreWith = function(cb){
@@ -24,14 +27,54 @@ var getStoreWith = function(cb){
 };
 
 var emptyFunc = function(){};
+//only call *Relation functions...
 
+// main record bucket: student, relation: exam
 junctionrelationstest.addBatch({
-  'fetch requests': {
-    topic: function(){
-      var store = getStoreWith(this.callback);
+  'fetchRelation given a relation with': {
+    
+    'isMaster and isDirectRelation set to true': {
+      topic: function(){
+        var store = getStoreWith(this.callback);
+        var req = createRequest();
+        var sr = makeStoreReq(req,C.ACTION_FETCH);
+        sr.relations[0].type = 'toMany';
+        sr.relations[0].isMaster = true;
+        sr.relations[0].isDirectRelation = true;
+        store.fetchRelation(sr,{id: 1}, sr.relations[0],emptyFunc);
+        return true;
+      },
+
+      // this is a way to test whether the callback is called when it should not. 
+      // vows doesn't seem to have an official way of making this sure.
+      // I expect this to work all the time, because vows will first finish the topic function, which would automatically
+      // fire the callback first, before returning true.
+      // it seems that it needs a separate batch to work properly though
+      'should not call the fetchDBRecords function': function(t){ 
+        assert.isTrue(t);
+      }
+    }
+  }
+}).addBatch({
+
+  'fetchRelation given an relation with' : {
+    'isChildRecord set to true and type toOne should retrieve a single record': {
+      topic: function(){
+        var store = getStoreWith(this.callback);
+        var req = createRequest();
+        var sr = makeStoreReq(req,C.ACTION_FETCH);
+        sr.relations[0].type = 'toOne';
+        sr.relations[0].keys = 1;
+        sr.relations[0].isMaster = true;
+        sr.relations[0].isDirectRelation = true;
+        store.fetchRelation(sr,{id: 1}, sr.relations[0],emptyFunc);
+      },
       
-      var sR = makeStoreReq(createRequest())
-      store.fetchRelation()
+      'should call retrieveDBRecord with the relation request': function(t){
+        assert.strictEqual(t.get('requestType'), C.ACTION_REFRESH);
+        assert.equal(t.key, 1);
+        assert.strictEqual(t.bucket,relationBucket);
+      }
     }
   }
 }).run();
